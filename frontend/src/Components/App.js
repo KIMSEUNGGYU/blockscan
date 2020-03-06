e93baf1c6619ec2ec58f5fafd0eeea20ef531a32
Opdata <pb03641@gmail.com>
2020-03-06 19:48:13 +0900

    [feature] 기본 스타일링 초기화 및 초기 설정
    - styled-components createGlobalStyle, styled-reset reset 사용으로 전역 스타일 초기화
    - styled-components ThemeProvider 사용으로 전역 스타일 불러올 수 있도록 설정

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../Styles/GlobalStyle';
+import Theme from '../Styles/Theme';
 
-function App() {
-  return <div></div>;
-}
+const Wrapper = styled.div``;
 
+const App = () => {
+  return (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Wrapper></Wrapper>
+    </ThemeProvider>
+  );
+};
 export default App;
