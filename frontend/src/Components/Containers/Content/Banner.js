bf492f627bc681c19ac4ea3f43e40fed82509661
Opdata <pb03641@gmail.com>
2020-03-11 02:16:42 +0900

    [feature] 기초적인 Banner 컴포넌트 추가

---
+++ frontend/src/Components/Containers/Content/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BannerDiv = styled.div`
+  height: 76.5px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+`;
+
+const BannerBox = styled.div`
+  width: 967px;
+  height: 100%;
+  border: 1px solid black;
+  background-color: white;
+`;
+
+export default () => {
+  return (
+    <BannerDiv>
+      <BannerBox />
+    </BannerDiv>
+  );
+};
