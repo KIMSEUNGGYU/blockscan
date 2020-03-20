edbeaf354ef5c1f844c39ccb19d00543a68d5282
JeongHaeMin <kjhm0607@gmail.com>
2020-03-21 00:05:21 +0900

    [fix, feat] /txs 에러처리, /txs/{txhash} 기능 구현
    - /txs 데이터 없는 경우 예외처리 추가
    - /txs/{txhash} API 구현

--- backend/controllers/txs.js
+++ backend/controllers/txs.js
@@ -1,5 +1,5 @@
-const { parseSpecificTxs } = require('../services/txs')
-const { viewTxs } = require('../views/txs')
+const { parseSpecificTxs, parseTxHashInfo } = require('../services/txs')
+const { viewTxs, viewTxInfo } = require('../views/txs')
 const { viewWrongRequest, viewNotFoundData } = require('../views/error')
 
 const controllerTxs = async (req, res, next) => {
@@ -12,11 +12,27 @@ const controllerTxs = async (req, res, next) => {
     };
     
     const txs = await parseSpecificTxs(p, pn);
+    if (txs.length == 0) {
+        res.json(viewNotFoundData());
+        return;
+    }
 
 
     res.json(viewTxs(txs))
 }
 
+const controllerTxDetail = async (req, res, next) => {
+    txHash = req.params.txHash
+    
+    const txHashInfo = await parseTxHashInfo(txHash)
+    if (!txHashInfo.length) {
+        res.json(viewNotFoundData())
+        return;
+    }
+    res.json(viewTxInfo(txHashInfo[0]))
+}
+
 module.exports = {
     controllerTxs,
+    controllerTxDetail
 }
