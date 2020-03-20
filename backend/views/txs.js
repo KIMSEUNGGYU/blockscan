e4f7adf9d150bea5d3c299150b3c593dff171d01
JeongHaeMin <kjhm0607@gmail.com>
2020-03-21 00:06:28 +0900

    [feat] tx detail info view 구현

--- backend/views/txs.js
+++ backend/views/txs.js
@@ -4,11 +4,26 @@ const viewTxs = (txs) => {
         status: 'success',
         message: 'Transaction list response',
         result: {
-        txs,
+            txs,
         },
         total: txs.length,
     };
 };
+
+
+const viewTxInfo = (txInfo) => {
+    return {
+        code: 200,
+        status: 'success',
+        message: 'Transaction Info response',
+        result: {
+            txInfo,
+        },
+    };
+}
+
+
 module.exports = {
     viewTxs,
+    viewTxInfo
 };
