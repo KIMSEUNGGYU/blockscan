e55c4b140fbe459e3583992a49e2005167099648
kimseunggyu <dev.seunggyu@gmail.com>
2020-03-24 04:13:12 +0900

    [feat] timestamp 를 시간으로 표현하는 기능 구현

--- frontend/src/helper/translate.js
+++ frontend/src/helper/translate.js
@@ -5,4 +5,47 @@ function hexToString(hex) {
   }
   return string;
 }
-export default hexToString;
+
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+
+function translateTimestamp(timestamp) {
+  let time = Date.now() - timestamp * SECOND;
+
+  let times = [];
+  times.push(parseInt(time / DAY));
+  time %= DAY;
+  times.push(parseInt(time / HOUR));
+  time %= HOUR;
+  times.push(parseInt(time / MINUTE));
+  time %= MINUTE;
+  times.push(parseInt(time / SECOND));
+
+  let count = 0;
+  let timeString = '';
+  if (times[0]) {
+    timeString += times[0] + ' days ';
+    count += 1;
+  }
+  if (times[1]) {
+    timeString += times[1] + ' hrs ';
+    count += 1;
+  }
+
+  if (times[2] && count < 2) {
+    timeString += times[2] + ' mins ';
+    count += 1;
+  }
+  if (times[3] && count < 2) {
+    timeString += times[3] + ' secs';
+  }
+
+  timeString += ' ago';
+
+  // console.log(timeString);
+  return timeString;
+}
+
+export { hexToString, translateTimestamp };
