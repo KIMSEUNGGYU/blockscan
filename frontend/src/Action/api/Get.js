d4ff5e8497c7f95b1619e495e2e3886309f55a38
Lee Sung Jun <34171875+Opdata@users.noreply.github.com>
2020-03-24 13:32:57 +0900

    [feat] 코드 최적화 (#86) (#88)
    - [refactor]코드 최적화

--- frontend/src/Action/api/Get.js
+++ frontend/src/Action/api/Get.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import qs from 'qs';
 import * as types from '../ActionTypes';
 
 const GetBlocks = data => {
@@ -11,31 +12,16 @@ const GetTxs = data => {
   return txs;
 };
 
-// const GetBlockAll = data => {
-//   const { blocks } = data.blocks;
-//   // console.log('전체블록', blocks);
-//   return blocks;
-// };
-
-// const GetTxAll = data => {
-//   const { txs } = data.txs;
-//   // console.log('전체Tx', txs);
-//   return txs;
-// };
-
 export const GetApi = async action => {
   let Data;
-  // console.log('GetApi호출'); //됨
   const result = await axios.get('http://49.50.162.172/api/v1');
   const data = result.data;
   switch (action) {
     case types.LATESTBLOCKS: {
-      // console.log('여기들어옴');
       Data = GetBlocks(data);
       break;
     }
     case types.LATESTTXS: {
-      // console.log('들어옴');
       Data = GetTxs(data);
       break;
     }
@@ -45,25 +31,22 @@ export const GetApi = async action => {
   return Data;
 };
 
-//All 요청주소로 수정해야함
-// export const GetAll = async (action, p, pn) => {
-//   let Data;
-//   // console.log('GetApi호출'); //됨
-//   const result = await axios.get('http://49.50.162.172/api/v1');
-//   const data = result.data;
-//   switch (action) {
-//     case types.BLOCKSALL: {
-//       // console.log('AllBlock');
-//       Data = GetBlockAll(data);
-//       break;
-//     }
-//     case types.TXSALL: {
-//       console.log('Alltxs');
-//       Data = GetTxAll(data);
-//       break;
-//     }
-//     default:
-//       break;
-//   }
-//   return Data;
-// };
+export const GetAll = async (action, p, pn) => {
+  let Data;
+  const result = await axios.get('http://49.50.162.172/api/v1/blocks?' + qs.stringify({ p, pn }));
+  const data = result.data;
+  switch (action) {
+    case types.BLOCKSALL: {
+      Data = GetBlocks(data);
+      break;
+    }
+    case types.TXSALL: {
+      console.log('Alltxs');
+      Data = GetTxs(data);
+      break;
+    }
+    default:
+      break;
+  }
+  return Data;
+};
